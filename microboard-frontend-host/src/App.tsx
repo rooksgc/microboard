8ee9e4b70aaa5bd11c3cfd918f00d932ce3b6bb9
rooksgc <rooksgc@gmail.com>
2025-10-01 17:46:12 +0300

    rebuilding

--- microboard-frontend-host/src/App.tsx
+++ microboard-frontend-host/src/App.tsx
@@ -1,15 +1,16 @@
 import type { FC } from "react";
-import { lazy, Suspense } from "react";
+// import { lazy Suspense } from "react";
 
-const ChildComponent = lazy(() => import("MicroboardFrontendChild/App"));
+// const ChildComponent = lazy(() => import("MicroboardFrontendChild/App"));
 
 interface AppProps {}
 
 const App: FC<AppProps> = () => {
   return (
-    <Suspense fallback={<div>Loading MicroboardChild Microfrontend...</div>}>
-      <ChildComponent />
-    </Suspense>
+    <p>ChildComponent (mock)</p>
+    // <Suspense fallback={<div>Loading MicroboardChild Microfrontend...</div>}>
+      //  <ChildComponent /> 
+    // </Suspense>
   );
 };
 
--- microboard-frontend-host/vite.config.ts
+++ microboard-frontend-host/vite.config.ts
@@ -1,22 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import federation from "@originjs/vite-plugin-federation";
+// import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
   plugins: [
     react(),
-    federation({
-      name: "microboard-frontend-host",
-      // The remotes we want to consume
-      remotes: {
-        MicroboardFrontendChild: {
-          external: "http://localhost:3001/assets/remoteEntry.js",
-          externalType: "url",
-        },
-      },
-      // Dependencies to share
-      shared: ["react", "react-dom"],
-    }),
+    // federation({
+    //   name: "microboard-frontend-host",
+    //   // The remotes we want to consume
+    //   remotes: {
+    //     MicroboardFrontendChild: {
+    //       external: "http://localhost:3001/assets/remoteEntry.js",
+    //       externalType: "url",
+    //     },
+    //   },
+    //   // Dependencies to share
+    //   shared: ["react", "react-dom"],
+    // }),
   ],
   preview: {
     host: "localhost",
