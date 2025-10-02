afcc0cadde0a2ebc112570a3ef444cec25327290
rooksgc <rooksgc@gmail.com>
2025-10-02 17:07:26 +0300

    https added

--- microboard-backend/main.py
+++ microboard-backend/main.py
@@ -30,7 +30,7 @@ def get_items():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "rooksgc.ru",
+        "https://rooksgc.ru",
     ],
     allow_credentials=True,
     allow_methods=["*"],
